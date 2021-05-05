2a71c892394799aa98547bb9685da2e76e3199f7
Atul Anand <atulanand206@gmail.com>
2021-05-05 06:31:15 +0530

    :punch: Exposing APIs with DB Connections.

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/atulanand206/go-mongo
+
+go 1.15
+
+require go.mongodb.org/mongo-driver v1.5.1
---
+++ mongo.go
@@ -0,0 +1,102 @@
+package mongo
+
+import (
+	"context"
+	"log"
+
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+)
+
+var clientUrl string
+
+func ConfigureMongoClient(url string) {
+	clientUrl = url
+}
+
+func GetMongoClient() *mongo.Client {
+	if clientUrl == "" {
+		log.Print("Client id not present")
+		return nil
+	}
+	client, err := mongo.NewClient(options.Client().ApplyURI(
+		clientUrl,
+	))
+	if err != nil {
+		log.Print(err)
+		return nil
+	}
+	return client
+}
+
+func Write(dbName string, collection string, doc bson.D) *mongo.InsertOneResult {
+	client := GetMongoClient()
+	ctx := context.Background()
+	client.Connect(ctx)
+	defer client.Disconnect(ctx)
+	db := client.Database(dbName)
+	dbCollection := db.Collection(collection)
+	result, err := dbCollection.InsertOne(ctx, doc)
+	if err != nil {
+		log.Print(err)
+		return nil
+	}
+	return result
+}
+
+func FindOne(dbName string, collection string, filter bson.M) *mongo.SingleResult {
+	client := GetMongoClient()
+	ctx := context.Background()
+	client.Connect(ctx)
+	defer client.Disconnect(ctx)
+	db := client.Database(dbName)
+	dbCollection := db.Collection(collection)
+	singleResult := dbCollection.FindOne(ctx, filter)
+	return singleResult
+}
+
+func Find(dbName string, collection string, filter bson.M) *mongo.Cursor {
+	client := GetMongoClient()
+	ctx := context.Background()
+	client.Connect(ctx)
+	defer client.Disconnect(ctx)
+	db := client.Database(dbName)
+	dbCollection := db.Collection(collection)
+	singleResult, err := dbCollection.Find(ctx, filter)
+	if err != nil {
+		log.Print(err)
+		return nil
+	}
+	return singleResult
+}
+
+func Update(dbName string, collection string, identifier bson.M, change bson.D) *mongo.UpdateResult {
+	client := GetMongoClient()
+	ctx := context.Background()
+	client.Connect(ctx)
+	defer client.Disconnect(ctx)
+	db := client.Database(dbName)
+	dbCollection := db.Collection(collection)
+	result, err := dbCollection.UpdateMany(ctx, identifier, change)
+	if err != nil {
+		log.Print(err)
+		return nil
+	}
+	return result
+}
+
+func Delete(dbName string, collection string, identifier bson.M) *mongo.DeleteResult {
+	client := GetMongoClient()
+	ctx := context.Background()
+	client.Connect(ctx)
+	defer client.Disconnect(ctx)
+	db := client.Database(dbName)
+	dbCollection := db.Collection(collection)
+	result, err := dbCollection.DeleteMany(ctx, identifier)
+	if err != nil {
+		log.Print(err)
+		return nil
+	}
+	return result
+}
